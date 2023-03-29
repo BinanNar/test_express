aa8c0440e22b346d9604d2ecc97802e710391c4c
BinanNar <halflifemaster1234@gmail.com>
2023-03-29 16:13:58 -0700

    proyecto crud

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const mysql   = require('mysql')
+const app     = express()
+const port    = 3000
+
+app.use(express.json())
+var con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"example",
+    database:"test"
+
+})
+
+con.connect( function (err){
+    if (err){ throw err}
+        console.log("Se conecto")
+})
+
+app.listen(port, () =>{
+    console.log('la app escucha el puerto '+port)
+})
+
+app.get("/alumnos",(req,res) => {
+    con.query("select * from alumno",
+    function (err,result){
+        if(err) {throw err}
+        res.status(200).json(result)
+    }
+    )  }
+)
+
+app.post("/alumnos",(req,res) => {
+
+    const alumno = {
+            id:req.body.id,
+            nombre:req.body.nombre,
+            apepat:req.body.apepat,
+            apemat:req.body.apemat
+    }
+
+    let insert = "insert into alumno set ?"
+    con.query(insert,alumno,
+    function (err,result){
+        if(err) {throw err}
+        res.status(200).json({"resultado":"con exito"})
+    }
+    )  }
+)
+
+app.put("/alumnos/:id",(req,res) => {
+
+    const alumno = {
+            id:req.body.id,
+            apemat:req.body.apemat
+    }
+
+    const {apemat} = req.body
+    const {id} = req.params
+
+    let insert = "update alumno set apemat = ? where id = ?"
+    con.query(insert,[apemat,id],
+    function (err,result){
+        if(err) {throw err}
+        res.status(200).json({"resultado":"con exito"})
+    }
+    )  }
+)
+
+
+app.delete("/alumnos/:id",(req,res) => {
+
+    const {id} = req.params
+    let insert = "delete from alumno  where id = ?"
+    con.query(insert,[id],
+    function (err,result){
+        if(err) {throw err}
+        res.status(200).json({"resultado":"borrado con exito"})
+    }
+    )  }
+)
+
+
+app.get('/', function (req, res) {
+    res.send(""+req.body.id);
+    // conslose.log(req)
+  });
---
+++ tabla.sql
@@ -0,0 +1,6 @@
+create table alumno(
+id int not null primary key,
+nombre varchar(30) not null,
+apepat varchar(30) not null,
+apemat varchar(30) not null
+)
